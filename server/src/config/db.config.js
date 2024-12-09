c557c9f947a005ddaa66d32a047c46c5c02c4dc4
VIVEK KUMAR <vk281977@gmail.com>
2024-12-09 11:43:32 +0530

    update db.config.js

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
 	try {
-		await mongoose.connect(process.env.DB_URI);
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("Connected to the database successfully");
 	} catch (error) {
 		console.error("Error connecting to the database", error);
